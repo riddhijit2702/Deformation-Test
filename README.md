b4cde1783e8a8e60e4cf7a92bef9d67fa39aa6d6
riddhijit2702 <74096278+riddhijit2702@users.noreply.github.com>
2020-11-12 11:24:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Deformation-Test
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,36 @@
+var car,wall;
+var speed,weight,deformation;
+
+
+
+
+function setup() {
+  createCanvas(1500,400);
+  speed=Math.round(random(55,90))
+  weight=Math.round(random(400,1600))
+ car= createSprite(200, 180, 50, 50);
+ car.velocityX=speed;
+ car.shapeColor="green"
+ wall=createSprite(1500,200,50,150)
+
+ 
+}
+
+function draw() {
+  background("black");
+  if(wall.x-car.x<car.width/2+wall.width/2)  {
+car.setVelocity(0,0)
+ deformation=speed*weight*0.5/22500;
+  }
+  if(deformation>180){
+   car.shapeColor=color(255,0,0)
+  }
+ if(deformation<180&&deformation>100){
+  car.shapeColor=color(230,230,0)
+  }
+ if(deformation<100){
+  car.shapeColor=color(0,255,0)
+ }
+
+  drawSprites();
+} 
